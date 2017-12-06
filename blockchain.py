033683c0becbc6d7680b943a4e0dc5f97b437ee5
the07 <thegauravks@gmail.com>
2017-12-06 12:50:17 +0530

    First commit

---
+++ blockchain.py
@@ -0,0 +1,67 @@
+import hashlib
+import logging
+import threading
+from time import time
+
+from user import *
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+class Peoplechain():
+
+    users = []
+
+    def __init__(self, users=None):
+
+        self.users_lock = threading.Lock()
+
+        if users is None:
+
+            genesis_user = self.get_genesis_user()
+            self.add_user(genesis_user)
+
+        else:
+
+            for user in users:
+                self.add_user(user)
+
+    def get_genesis_user(self):
+
+        print ("###########################################################\n")
+        print ("#################### CREATING GENESIS USER ################\n")
+
+        genesis_user_address = '0409eb9224f408ece7163f40a33274d99ab6b3f60e41b447dd45fcc6371f57b88d9d3583c358b1ea8aea4422d17c57de1418554d3a1cd620ca4cb296357888ea596'
+        genesis_user_name = 'Network'
+        genesis_user_balance = 100000000000000
+        genesis_user_data = {
+            "Location": "Bangalore"
+        }
+
+        genesis_user = User(genesis_user_address, genesis_user_name, genesis_user_balance, genesis_user_data)
+        print ("#################### GENESIS USER CREATED ################\n")
+        return genesis_user
+
+    def add_user(self, user):
+        with self.users_lock:
+            self.users.append(user)
+            return True
+        return False
+
+    def get_size(self):
+        return len(self.users)
+
+    def get_user_by_address(self, address):
+        for user in self.users:
+            if user.address == address:
+                return user
+        return False
+
+    def get_all_users(self):
+        return self.users
+
+    def __str__(self):
+        return str(self.__dict__)
+
+if __name__ == '__main__':
+    pass
---
+++ user.py
@@ -0,0 +1,61 @@
+import json
+
+class User:
+
+    def __init__(self, address, name, balance=None, data=None):
+        # Add option for a signature ( For security )
+        self._address = address
+        self._name = name
+        if balance is not None:
+            self._balance = balance
+        else:
+            self._balance = 100
+        if data is not None:
+            self._data = data
+        else:
+            self._data = {}
+
+    @property
+    def address(self):
+        return self._address
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def balance(self):
+        return self._balance
+
+    @property
+    def data(self):
+        return self._data
+
+    @classmethod
+    def from_json(cls, user_json):
+        user = cls.__init__(user_json['address'], user_json['name'], user_json.get('balance', None), user_json.get('data', None))
+        return user
+
+    def setname(self, value):
+        self._name = value
+        return
+
+    def setbalance(self, value):
+        self._balance = value
+        return
+
+    def setdata(self, value):
+        self._data = value
+        return
+
+    def to_json(self):
+        return json.dumps(self, default=lambda o: {key.lstrip('_'): value for key, value in o.__dict__.items()}, sort_keys=True)
+
+    def __repr__(self):
+        return "<User {}>".format(self._address)
+
+    def __str__(self):
+        return str(self.__dict__)
+
+if __name__ == '__main__':
+    pass
